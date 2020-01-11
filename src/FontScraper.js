8fdb30f9fe1c19f55d763527dc2ea2630d5e76ac
harmendv@gmail.com <%3eXcGcJZ1BmM%rzEDL48o>
2020-01-11 21:01:29 +0100

    Improved Regex. Add scan for google font links

--- src/FontScraper.js
+++ src/FontScraper.js
@@ -4,8 +4,12 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const colors = require('colors');
+const colors = require('colors'); // exposes String().color
 
+// Detect any links inside href, src, data-src, url()
+const REGEX_FONTS = new RegExp(/(((?<=href=["']))|(?<=src=["'])|(?<=data-src=["']))(\S+)(?=["'])|(?<=url\(['"])(\S+)(?=['"])|((?<=url\()[a-zA-Z0-9:/_.-]+)(?=\))/gim);
+
+// Allowed font file extensions
 const FONT_FILE_EXTENSIONS = [
   '.ttf',
   '.otf',
@@ -18,12 +22,12 @@ const FONT_FILE_EXTENSIONS = [
 class FontScraper {
   constructor(args) {
     this.arguments = args;
-    this.url;
-    this.base_url;
-    this.html;
-    this.links;
-    this.fonts;
-    this.output;
+    this.url = null;
+    this.base_url = null;
+    this.html = null;
+    this.links = [];
+    this.fonts = [];
+    this.output = null;
 
     // Check if an URL is given in the second argument
     if (this.arguments[2]) {
@@ -69,8 +73,8 @@ class FontScraper {
     })
   }
 
-  getLinksFromHTML(html) {
-    var links = html.match(/(((?<=href=["']))|(?<=src=["'])|(?<=data-src=["']))(\S+)(?=["'])/gim);
+  getLinksFromString(html) {
+    var links = html.match(REGEX_FONTS);
     var modifiedLinks = [];
     if (!links) {
       return [];
@@ -85,7 +89,7 @@ class FontScraper {
         }
       });
     }
-    console.log('Found ' + String(modifiedLinks.length).green + ' links.');
+    console.log('Analyzed ' + String(modifiedLinks.length).green + ' links.');
     // if(modifiedLinks.length > 0) {
     //   console.log(modifiedLinks);
     // }
@@ -93,7 +97,7 @@ class FontScraper {
   }
 
   getLinksFromCSS(css) {
-    var links = css.match(/(?<=url\(['"])(\S+)(?=['"])|((?<=url\()[^"|^'])(\S+)(?=\))/gim);
+    var links = css.match(REGEX_FONTS);
     var modifiedLinks = [];
     if (!links) {
       return [];
@@ -111,36 +115,46 @@ class FontScraper {
     return modifiedLinks;
   }
 
-  findCSSinHTML(links) {
+  filterCssUrls(urls) {
     let css = [];
-    links.forEach((link) => {
-      if (link.includes('css')) {
-        css.push(link);
+    urls.forEach((url) => {
+      if (url.includes('css')) {
+        css.push(url);
       }
     })
-    console.log('Found ' + String(css.length).green + ' CSS file(s)');
-    // if(css.length > 0) {
-    //   console.log(css);
-    // }
+    console.log('Found ' + String(css.length).green + ' CSS files');
     return css;
   }
 
-  findFontsInHTML(links) {
+  areUrlsFonts(urls) {
     return new Promise((resolve, reject) => {
       let fonts = [];
-      links.forEach((link) => {
-        const extension = path.extname(link);
+      urls.forEach((url) => {
+        const extension = path.extname(url);
         if (FONT_FILE_EXTENSIONS.includes(extension)) {
-          fonts.push(link);
+          fonts.push(url);
         }
       })
       resolve(fonts);
     })
   }
 
-  findFontsInCSS(links) {
+  findGoogleFontUrls(urls) {
+    return new Promise((resolve) => {
+      let googleFontUrls = [];
+      urls.forEach((url) => {
+        if (url.includes('fonts.googleapis.com')) {
+          googleFontUrls.push(url);
+        }
+      });
+      resolve(googleFontUrls);
+    })
+
+  }
+
+  findFontsInCssUrls(urls) {
     return new Promise((resolve, reject) => {
-      const filePaths = this.findCSSinHTML(links);
+      const filePaths = this.filterCssUrls(urls);
       const fonts = [];
       const getLinksPromises = [];
       const getContentPromises = [];
@@ -171,18 +185,30 @@ class FontScraper {
 
   getFonts() {
     this.getContents(this.url).then((body) => {
-      const links = this.getLinksFromHTML(body);
-      Promise.all([this.findFontsInHTML(links), this.findFontsInCSS(links)]).then(results => {
+      const urls = this.getLinksFromString(body);
+      Promise.all([this.areUrlsFonts(urls), this.findFontsInCssUrls(urls), this.findGoogleFontUrls(urls)]).then(results => {
         // results[0] is HTML
         console.log('Found ' + String(results[0].length).green + ' fonts in directly the HTML.')
         if(results[0].length > 0) {
-          console.log(results[0])
+          results[0].forEach((url) => {
+            console.log(`- ${url}`)
+          })
         }
         // results[1] is CSS
         console.log('Found ' + String(results[1].length).green + ' fonts in CSS files.')
         if(results[1].length > 0) {
-          console.log(results[1])
+          results[1].forEach((url) => {
+            console.log(`- ${url}`)
+          })
         }
+        // results[2] is Google Font Urls
+        console.log('Found ' + String(results[2].length).green + ' Google Font URLs.')
+        if(results[2].length > 0) {
+          results[2].forEach((url) => {
+            console.log(`- ${url}`)
+          })
+        }
+
         // Check if output is given, then download to the output!
         if(this.output) {
           console.log('Downloading all fonts.');
@@ -201,8 +227,8 @@ class FontScraper {
           })
         }
       })
-    }).catch(() => {
-      throw new Error('Something went wrong getting retrieving the URL');
+    }).catch((e) => {
+      throw new Error(`Something went wrong getting retrieving the URL: ${e}`);
     })
   }
 
